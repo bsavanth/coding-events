3c73c6b07de53e10c0133e1ca7bb35fc02bdfab6
bsavanth <bsavanth@uark.edu>
2021-03-22 09:57:01 -0500

    Added Dependency Injection

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -12,15 +12,8 @@ import java.util.Objects;
  * Created by Chris Bay
  */
 @Entity
-public class Event {
+public class Event extends AbstractEntiity{
 
-    @Id
-    @GeneratedValue
-    private int id;
-
-    @NotBlank(message = "Name is required")
-    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
-    private String name;
 
     @Size(max = 500, message = "Description too long!")
     private String description;
@@ -32,21 +25,13 @@ public class Event {
     private EventType type;
 
     public Event(String name, String description, String contactEmail, EventType type) {
-        this.name = name;
+        super(name);
         this.description = description;
         this.contactEmail = contactEmail;
         this.type = type;
     }
 
-    public Event() {}
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+    public Event() { super(); }
 
     public String getDescription() {
         return description;
@@ -72,25 +57,6 @@ public class Event {
         this.type = type;
     }
 
-    public int getId() {
-        return id;
-    }
 
-    @Override
-    public String toString() {
-        return name;
-    }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Event event = (Event) o;
-        return id == event.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
--- src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -8,53 +8,17 @@ import javax.validation.constraints.Size;
 import java.util.Objects;
 
 @Entity
-public class EventCategory {
-
-    @Id
-    @GeneratedValue
-    private int id;
-
-    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
-    private String name;
+public class EventCategory extends AbstractEntiity{
 
     EventCategory(@Size(min = 3, message = "Name must be at least 3 characters long") String name)
     {
-        this.name=name;
+        super(name);
     }
 
     public EventCategory()
     {
-
+        super();
     }
 
-    public int getId() {
-        return id;
-    }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString()
-    {
-        return name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof EventCategory)) return false;
-        EventCategory that = (EventCategory) o;
-        return id == that.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
